f637be770b0cba3fac36fa212d2af6dddc9cd434
rlongstaff <rob@longstaff.us>
2025-08-06 07:27:24 -0400

    Make gcloud env shell aware and not teh dumbs

--- dotshell/common.d/gcloud.sh
+++ dotshell/common.d/gcloud.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-BASH_INC=(
-    ${HOME}/prj/google-cloud-sdk/path.bash.inc
-    ${HOME}/prj/google-cloud-sdk/completion.bash.inc
-)
+if [ -n "$ZSH_VERSION" ]; then
+    GCLOUD_INC=(
+        ${HOME}/prj/google-cloud-sdk/path.zsh.inc
+        ${HOME}/prj/google-cloud-sdk/completion.zsh.inc
+    )
+elif [ -n "$BASH_VERSION" ]; then
+    GCLOUD_INC=(
+        ${HOME}/prj/google-cloud-sdk/path.bash.inc
+        ${HOME}/prj/google-cloud-sdk/completion.bash.inc
+    )
+fi
 
-for i in ${BASH_INC[@]}; do
+for i in ${GCLOUD_INC[@]}; do
     if [ -f ${i} ]; then
         source ${i}
     fi
 done
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Volumes/prj/google-cloud-sdk/path.zsh.inc' ]; then . '/Volumes/prj/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Volumes/prj/google-cloud-sdk/completion.zsh.inc' ]; then . '/Volumes/prj/google-cloud-sdk/completion.zsh.inc'; fi
