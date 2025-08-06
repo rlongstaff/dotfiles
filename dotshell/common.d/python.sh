4bd74afb1adc5f8dd711415829f275a1d3fab756
rlongstaff <rob@longstaff.us>
2025-08-06 09:44:30 -0400

    stop python virtualenv from mucking with the prompt

--- dotshell/common.d/python.sh
+++ dotshell/common.d/python.sh
@@ -2,6 +2,8 @@
 
 alias python=python3
 
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
 if [ -f ${HOME}/prj/virtualenv/main/bin/activate ]; then
     source ${HOME}/prj/virtualenv/main/bin/activate
 elif [ -f ${HOME}/prj/virpy/main/bin/activate ]; then
