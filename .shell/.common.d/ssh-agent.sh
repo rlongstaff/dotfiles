8e26a5302c5c0b99623d48bf9ce0cc92a467ee7c
rlongstaff <rob@longstaff.us>
2025-08-13 04:23:47 -0400

    make ssh-agent more reliable

--- .shell/.common.d/ssh-agent.sh
+++ .shell/.common.d/ssh-agent.sh
@@ -1,15 +1,15 @@
 
+
 AGENT_FILE="$HOME/.ssh/agent.env"
-AGENT_CMD="/usr/bin/ssh-agent -s"
+AGENT_CMD=(/usr/bin/ssh-agent -s)
 
 start_ssh_agent() {
-       if [ -f $AGENT_FILE -a -z "$SSH_AGENT_PID" ]; then
+       if [ -f $AGENT_FILE -a -z "$SSH_AUTH_SOCK" ]; then
                source $AGENT_FILE
        fi
-       /bin/ps -p $SSH_AGENT_PID >& /dev/null
-       if [ $? != 0 ]; then
+       if [ ! -e $SSH_AUTH_SOCK ]; then
                echo "Stale agent. Killing pidfile."
-               $AGENT_CMD | grep -v echo > $AGENT_FILE 2>/dev/null
+               "$AGENT_CMD[@]" | grep -v echo > $AGENT_FILE 2>/dev/null
                source $AGENT_FILE
                /bin/chmod 600 $AGENT_FILE
        fi
