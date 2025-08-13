e9a7ffecb91fe87edf241e0c4cf0d946d2f3313f
rlongstaff <rob@longstaff.us>
2025-08-12 21:56:43 -0400

    vscode aliases

--- dotshell/common.d/vscode.sh
+++ dotshell/common.d/vscode.sh
@@ -1,2 +1,16 @@
 
-export PATH=$PATH:/Applications/Development/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+export PATH="${PATH}:/Applications/Development/Visual\ Studio\ Code.app/Contents/Resources/app/bin"
+
+function vsc {
+  if (( $# )); then
+    $VSCODE $@
+  else
+    $VSCODE .
+  fi
+}
+
+alias vsca="$VSCODE --add"
+alias vscd="$VSCODE --diff"
+alias vscg="$VSCODE --goto"
+alias vscn="$VSCODE --new-window"
+alias vscr="$VSCODE --reuse-window"
