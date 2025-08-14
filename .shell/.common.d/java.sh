6892ed67b910f5afa71b43a4992a540d5575afba
rlongstaff <rob@longstaff.us>
2025-08-13 20:25:58 -0400

    Add CPP flags for openjdk managed my homebrew

--- .shell/.common.d/java.sh
+++ .shell/.common.d/java.sh
@@ -1,2 +1,10 @@
 
+# Using OpenJDK installed via Homebrew
+
 export PATH="/usr/local/opt/openjdk/bin:$PATH"
+
+# For the system Java wrappers to find this JDK, symlink it with
+#   sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+# For compilers to find openjdk you may need to set:
+export CPPFLAGS="-I/usr/local/opt/openjdk/include"
