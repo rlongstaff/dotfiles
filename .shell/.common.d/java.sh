ce9a9de28de5d47e6caa09f6dc996050c7908a8b
rlongstaff <rob@longstaff.us>
2025-09-14 07:35:40 -0400

    remove openjdk include files from default cpp_include

--- .shell/.common.d/java.sh
+++ .shell/.common.d/java.sh
@@ -7,4 +7,4 @@ export PATH="/usr/local/opt/openjdk/bin:$PATH"
 #   sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 # For compilers to find openjdk you may need to set:
-export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+# export CPPFLAGS="-I/usr/local/opt/openjdk/include"
