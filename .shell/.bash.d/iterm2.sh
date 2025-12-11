f83b71c0599de4426e5acbc47b4464b45bb61872
rlongstaff <rob@longstaff.us>
2025-12-11 09:35:24 -0500

    fix bash iterm include

--- .shell/.bash.d/iterm2.sh
+++ .shell/.bash.d/iterm2.sh
@@ -2,6 +2,6 @@
 
 if [ -e "${HOME}/.iterm2_shell_integration.bash" ]; then
     source "${HOME}/.iterm2_shell_integration.bash"
-elif [ ${LC_TERMINAL} -eq "iTerm2" ]; then
+elif [ ${LC_TERMINAL} = "iTerm2" ]; then
     echo "iTerm2 shell integration not found. Please install it from iTerm2 preferences."
 fi
