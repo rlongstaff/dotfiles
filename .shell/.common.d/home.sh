bbec54f0a794faabc869f6ed0daf672001bd289d
rlongstaff <rob@longstaff.us>
2025-12-16 16:15:52 -0500

    fix less params

--- .shell/.common.d/home.sh
+++ .shell/.common.d/home.sh
@@ -3,12 +3,14 @@ export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/bin:/usr/local/bin:$PATH"
 
 export MANPATH="/usr/local/man:$MANPATH"
+export LESS="-EXFR"
 
 export CLICOLOR=1
 export LSCOLORS=Exfxdxdxcxegedabagacad
 export LS_COLORS="di=1;34:ln=35:so=33:pi=33:ex=32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
 export EDITOR=/usr/bin/vim
+
 # Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
 #   export EDITOR='vim'
