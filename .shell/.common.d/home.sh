5ae07947941926cbfb3ea689bff8e476777569d0
rlongstaff <rob@longstaff.us>
2026-08-25 00:12:43 -0400

    remove full path from editor=vim

--- .shell/.common.d/home.sh
+++ .shell/.common.d/home.sh
@@ -9,7 +9,7 @@ export CLICOLOR=1
 export LSCOLORS=Exfxdxdxcxegedabagacad
 export LS_COLORS="di=1;34:ln=35:so=33:pi=33:ex=32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
-export EDITOR=/usr/bin/vim
+export EDITOR=vim
 
 # Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
