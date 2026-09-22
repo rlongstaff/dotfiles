c4ee7e95bd162d7b7555bae2398a7fbf36409fa3
rlongstaff <rob@longstaff.us>
2026-09-22 15:34:48 -0400

    checkpoint

--- .shell/.common.d/home.sh
+++ .shell/.common.d/home.sh
@@ -1,6 +1,7 @@
 
 export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"
+export PATH="$PATH:$HOME/.local/bin:/usr/local/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
 
 export MANPATH="/usr/local/man:$MANPATH"
 export LESS="-EXFR"
